22cbd40fb3afcf4d2aeebac02572cdb8795485ef
filtoid <philjeffes@hotmail.com>
2023-11-29 14:18:55 +0100

    two objects can point at each other
    Also reset button to delete localstorage
    
    draw feet separately otherwise they will draw over arms

--- js/main.js
+++ js/main.js
@@ -3,20 +3,67 @@ let ctx = null;
 
 let ident = null;
 let character = null;
-let ROTATION = 0;
 
+let state = null;
+
+const LS_KEY_NAME = "cs_positions";
 const CHAR_LIST = ["andy", "michael", "dwight", "pam"];
+
+function get_state() {
+    let state = localStorage.getItem(LS_KEY_NAME);
+    if(state == null) {
+        state = {"chars": []}
+    } else {
+        state = JSON.parse(state);
+    }
+    return state;
+}
+
+function set_state(new_state) {
+    localStorage.setItem(LS_KEY_NAME, JSON.stringify(new_state));
+}
+
+function reset_state() {
+    localStorage.removeItem(LS_KEY_NAME);
+    location.reload();
+}
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
+  
+    state = get_state();
 
-    let rand = Math.floor(Math.random() * 4);
-    
-    character = CHAR_LIST[rand];
     ident = crypto.randomUUID();
 
-    console.log(character, ident);
+    if(state.chars.length == CHAR_LIST.length) {
+        console.log("All characters accounted for");
+        reset_state();
+        return;
+    }
+
+    let quit = false;
+    while(quit != true) {
+        let rand = Math.floor(Math.random() * 4);
+        character = CHAR_LIST[rand];
+        let bFound = false;
+        for(var i=0;i<state.chars.length;i++){
+            let c = state.chars[i];
+            if(c.name == character) {
+                bFound = true;
+            }
+        }
+        if(!bFound) {
+            state.chars.push({
+                "id": ident,
+                "name": character,
+                "position": {"x": 0, "y": 0}
+            });
+            quit = true;
+        }
+    }
+
+    set_state(state);
 
     setInterval(update, 100);
 }
@@ -28,6 +75,23 @@ function update() {
 
     ctx.canvas.width  = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
+
+    let position = {x:  window.screenX + ctx.canvas.width/2, y: window.screenY + ctx.canvas.height/2};
+    let state = get_state();
+    if(state.chars.length == 0){
+        // Something has gone wrong - possibly another window nuked everything, so start again
+        reset_state();
+        return;
+    }
+    // Update the state stored for our character
+    for(var i=0;i<state.chars.length;i++){
+        if(state.chars[i].id == ident){
+            state.chars[i].position.x = position.x;
+            state.chars[i].position.y = position.y;
+            break;
+        }
+    }
+    set_state(state);
 
     ctx.fillRect(0,0,canvas.width, canvas.height);
     
@@ -56,26 +120,37 @@ function update() {
     let body = IMAGE_LOADER.get_image("assets/images/" + character + "/body.png");
     let left = IMAGE_LOADER.get_image("assets/images/" + character + "/left_arm.png");
     let right = IMAGE_LOADER.get_image("assets/images/" + character + "/right_arm.png");
+    let feet = IMAGE_LOADER.get_image("assets/images/" + character + "/feet.png");
 
-    if(body == null || left == null || right == null) {
+    if(body == null || left == null || right == null || feet == null) {
         // Image not ready yet
         return;
     }
-    // Draw right arm
-    // ctx.translate((canvas.width/2)-25, (canvas.height/2));
-    // ctx.translate((canvas.width/2)-25, (canvas.height/2));
-
-    // ctx.drawImage(right, (canvas.width/2) - 25, canvas.height/2);
     
-    ROTATION += 0.05;
+    // Calculate rotation
+    let pos = {x:0, y:0};
+    for(var i=0;i<state.chars.length;i++){
+        if(state.chars[i].id != ident) {
+            pos.x = state.chars[i].position.x;
+            pos.y = state.chars[i].position.y;
+        }
+    }
 
+    let rotation = get_angle_to_point(position, pos);
+
+    // Apply translation and rotation
     ctx.translate((canvas.width/2) + 50, (canvas.height/2) + 25);
-    ctx.rotate(ROTATION);
+    ctx.rotate(rotation);
     ctx.translate((-canvas.width/2) - 50, (-canvas.height/2) - 25 );
-
+    
+    // Draw the assets
+    ctx.drawImage(feet, canvas.width/2, canvas.height/2);
     ctx.drawImage(right, (canvas.width/2) + 10, canvas.height/2 + 10 );
     ctx.drawImage(left, (canvas.width/2) + 70, canvas.height/2 + 10 );
     ctx.drawImage(body, canvas.width/2, canvas.height/2);
 
-    
+}
+
+function get_angle_to_point(pt1, pt2) {
+    return Math.atan2(pt1.x - pt2.x, pt2.y-pt1.y);
 }
